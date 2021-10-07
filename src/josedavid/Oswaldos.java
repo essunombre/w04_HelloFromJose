294f6afc7949b161cf34afd9a5ab14c9296ac40c
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-10-07 17:24:21 -0400

    Oswaldo added a :

--- src/josedavid/Oswaldos.java
+++ src/josedavid/Oswaldos.java
@@ -4,7 +4,7 @@ public class Oswaldos {
 
     public void Prints(String myName){
         System.out.println("Hello, my name is "+ myName + "!");
-        System.out.println("What is your name?");
+        System.out.println("What is your name?: ");
         HiFriend myfriend = new HiFriend();
         System.out.println("Hello "+ myfriend.friend + "!");
         System.out.println("Oswaldo was just playing with your code");
