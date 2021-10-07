4098a32df8da39772cdbf0fcbe24112ee96d556c
essunombre <alb21001@byui.edu>
2021-10-07 14:12:00 -0500

    Initial commit

---
+++ src/josedavid/Main.java
@@ -0,0 +1,10 @@
+package josedavid;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String myName = "Jose";
+        System.out.println("Hello, my name is "+ myName + "!");
+    }
+}
