f3384f69348251aa2523d41d5237b09d6512a1fb
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-10-07 17:12:58 -0400

    oswaldo's changes

--- src/josedavid/Main.java
+++ src/josedavid/Main.java
@@ -3,10 +3,8 @@ package josedavid;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         String myName = "Jose";
         Oswaldos oswaldos = new Oswaldos();
         oswaldos.Prints(myName);
-
     }
 }
