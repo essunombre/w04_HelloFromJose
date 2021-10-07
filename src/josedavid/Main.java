d98337be8ffcab80acf411d893b311fdfdfef41c
essunombre <alb21001@byui.edu>
2021-10-07 14:56:45 -0500

    Add the printLn "what is your name"

--- src/josedavid/Main.java
+++ src/josedavid/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	// write your code here
         String myName = "Jose";
         System.out.println("Hello, my name is "+ myName + "!");
-
+        System.out.println("What is your name?");
         HiFriend myfriend = new HiFriend();
         System.out.println("Hello "+ myfriend.friend + "!");
     }
